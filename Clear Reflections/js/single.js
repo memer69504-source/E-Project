f9283f875fd4b2ce61b1eab78c5a65c4ecdb1a53
source_1133-atf <memer69504@gmail.com>
2026-04-08 14:11:52 +0500

    changes

--- Clear Reflections/js/single.js
+++ Clear Reflections/js/single.js
@@ -426,30 +426,35 @@ Reflects elegance and brightness.`,
     }
 ];
 let singleproduct = mirrors.find(function (items) {
-    return items.id ==productId
+    return items.id == productId
 })
 
 let container = document.getElementById('detail')
 if (!singleproduct) {
     container.innerHTML = "<h2>Product not found</h2>";
 } else {
-container.innerHTML = ` <div class="col-md-3">
-                <div id="productCarousel" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="${singleproduct.image}" class="d-block w-100 img-fluid" alt="Product Image 1">
-                        </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
+    container.innerHTML = ` <div class="col-md-3">
+          <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="${singleproduct.image}" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
+      <img src="..." class="d-block w-100" alt="...">
     </div>
-    
-                        
-                    </div>
-
-                </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
             </div>
             <div class="col-md-9">
                 <h1>${singleproduct.name}</h1>
